a10ef4f409b39d39cfff9052b188a80375c103b0
Caciqxe <96596445+Caciqxe@users.noreply.github.com>
2022-09-15 18:54:13 -0300

    subida desafio clase 9
    subida desafio clase 9

---
+++ Clase9_Cartes/color.js
@@ -0,0 +1,38 @@
+let cambio_color = document.getElementById("cambio_color");
+let titulo = document.getElementById("titulo");
+let elemento = document.querySelector('body');
+
+let abecedario = ['A','B','C','D','E','F','0','1','2','3','4','5','6','7','8','9'];
+
+//Posiciones Aleatorias
+
+function Aleatorio() {
+    let aleatorio1 = abecedario[Math.floor(Math.random()*abecedario.length)];
+    let aleatorio2 = abecedario[Math.floor(Math.random()*abecedario.length)];
+    let aleatorio3 = abecedario[Math.floor(Math.random()*abecedario.length)];
+    let aleatorio4 = abecedario[Math.floor(Math.random()*abecedario.length)];
+    let aleatorio5 = abecedario[Math.floor(Math.random()*abecedario.length)];
+    let aleatorio6 = abecedario[Math.floor(Math.random()*abecedario.length)];
+
+    let nuevo_color = '#'+aleatorio1+aleatorio2+aleatorio3+aleatorio4+aleatorio5+aleatorio6
+    //let Aleatorio = document.body.style.background()
+    elemento.style.background = nuevo_color
+    titulo.innerHTML = nuevo_color
+    console.log(nuevo_color);
+}
+
+cambio_color.addEventListener("click", Aleatorio);
+elemento.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+    document.getElementById("cambio_color").click();
+    }
+});
+
+
+
+
+console.log(cambio_color);
+
+
+//GENERA NÚMERO ALEATORIO ENTRE 0 Y 1
+console.log(Math.random());
